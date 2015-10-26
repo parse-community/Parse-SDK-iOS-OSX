8fc802029a27f7c08e5fbcedf6f49d5efcef6d04
Marc Knaup <marc@knaup.koeln>
2015-10-26 17:26:29 +0100

    Renamed Bolts.h to Parse+Bolts.h to prevent potential conflict when using CocoaPods

--- Parse/PFFile.h
+++ Parse/PFFile.h
@@ -9,7 +9,7 @@
 
 #import <Foundation/Foundation.h>
 
-#import "Bolts.h"
+#import "Parse+Bolts.h"
 
 #import "PFConstants.h"
 
--- Parse/PFSession.h
+++ Parse/PFSession.h
@@ -9,7 +9,7 @@
 
 #import <Foundation/Foundation.h>
 
-#import "Bolts.h"
+#import "Parse+Bolts.h"
 
 #import "PFObject.h"
 #import "PFSubclassing.h"
