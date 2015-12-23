8a75d584e3965da3d774e7a31f62ab4a55ea1a17
Nikita Lutsenko <nlutsenko@me.com>
2015-12-22 17:29:13 -0800

    Add documentation for deprecated categories.

--- Parse/PFQuery+Deprecated.h
+++ Parse/PFQuery+Deprecated.h
@@ -12,6 +12,9 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+/**
+ This category lists all methods of `PFQuery` that are deprecated and will be removed in the near future.
+ */
 @interface PFQuery (Deprecated)
 
 ///--------------------------------------
