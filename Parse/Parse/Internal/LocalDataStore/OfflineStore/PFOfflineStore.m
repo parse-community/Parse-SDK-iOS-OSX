a5ae14d08e9492802befb4471ba2f20c7e22e7ea
Ahmad Ishfaq <ahmad@trailsventures.com>
2016-06-09 23:33:51 +0500

    Added Notification on non existent UDID

--- Parse/Parse/Internal/LocalDataStore/OfflineStore/PFOfflineStore.m
+++ Parse/Parse/Internal/LocalDataStore/OfflineStore/PFOfflineStore.m
@@ -178,7 +178,9 @@ static int const PFOfflineStoreMaximumSQLVariablesCount = 999;
                                    PFOfflineStoreKeyOfJSON, PFOfflineStoreTableOfObjects, PFOfflineStoreKeyOfUUID];
                 return [database executeQueryAsync:query withArgumentsInArray:@[ uuid ] block:^id(PFSQLiteDatabaseResult *_Nonnull result) {
                     if (![result next]) {
-                        PFConsistencyAssertionFailure(@"Attempted to find non-existent uuid %@. Please report this issue with stack traces and logs.", uuid);
+                         [[NSNotificationCenter defaultCenter] postNotificationName:@"non-existent-uuid" object:uuid];
+                        [NSException raise:NSInternalInconsistencyException
+                                    format:@"Attempted to find non-existent uuid %@.", uuid];
                     }
                     return [result stringForColumnIndex:0];
                 }];
@@ -912,7 +914,9 @@ static int const PFOfflineStoreMaximumSQLVariablesCount = 999;
     __block NSString *objectId = nil;
     return [[database executeQueryAsync:query withArgumentsInArray:@[ uuid ] block:^id(PFSQLiteDatabaseResult *result) {
         if (![result next]) {
-            PFConsistencyAssertionFailure(@"Attempted to find non-existent uuid %@. Please report this issue with stack traces and logs.", uuid);
+             [[NSNotificationCenter defaultCenter] postNotificationName:@"non-existent-uuid" object:uuid];
+            [NSException raise:NSInternalInconsistencyException
+                        format:@"Attempted to find non-existent uuid %@", uuid]; // TODO: (nlutsenko) Convert to errors.
         }
 
         className = [result stringForColumnIndex:0];
