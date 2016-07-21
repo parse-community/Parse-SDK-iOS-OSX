7a820b75c6726808475af9ce705053ff0e8cbd11
Nikita Lutsenko <nlutsenko@me.com>
2016-07-21 15:23:48 -0700

    Parse 1.14.2 ☁️ (#988)

--- Parse/PFConstants.h
+++ Parse/PFConstants.h
@@ -13,7 +13,7 @@
 #pragma mark - SDK Version
 ///--------------------------------------
 
-#define PARSE_VERSION @"1.14.1"
+#define PARSE_VERSION @"1.14.2"
 
 ///--------------------------------------
 #pragma mark - Platform
