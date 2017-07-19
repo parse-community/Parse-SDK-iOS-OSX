737d85ddfe4c498a622ecd07cdfc7f7afc297045
Florent Vilmart <florent@flovilmart.com>
2017-07-19 11:30:39 -0400

    wip

---
+++ jazzy.sh
@@ -0,0 +1,10 @@
+jazzy \
+  --objc \
+  --author Parse \
+  --author_url http://parseplatform.org \
+  --github_url https://github.com/parse-community/Parse-SDK-iOS-OSX \
+  --module-version 1.15.0 \
+  --framework-root Parse \
+  --umbrella-header Parse/Parse.h \
+  --sdk iphoneos\
+  --module Parse
