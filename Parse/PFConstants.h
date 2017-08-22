37aee695cf654c519243adc2945161a089855029
Florent Vilmart <florent@flovilmart.com>
2017-08-21 20:41:52 -0400

    :zap: Release 1.15.2

--- Parse/PFConstants.h
+++ Parse/PFConstants.h
@@ -13,7 +13,7 @@
 #pragma mark - SDK Version
 ///--------------------------------------
 
-#define PARSE_VERSION @"1.15.1"
+#define PARSE_VERSION @"1.15.2"
 
 ///--------------------------------------
 #pragma mark - Platform
