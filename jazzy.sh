7d66bd8fb31385e6a722a951d75fee8cd51d678f
Florent Vilmart <florent@flovilmart.com>
2017-12-03 13:51:04 -0500

    Proper docs generation

--- jazzy.sh
+++ jazzy.sh
@@ -1,10 +1,21 @@
+mkdir -p ./Bolts # Create a temporary bolts folder
+cp -R Carthage/Checkouts/Bolts-ObjC/Bolts/**/*.h ./Bolts # Copy bolts
+
+ver=`/usr/libexec/PlistBuddy -c "Print CFBundleShortVersionString" Parse/Resources/Parse-iOS.Info.plist`
+
 jazzy \
   --objc \
   --author Parse \
   --author_url http://parseplatform.org \
   --github_url https://github.com/parse-community/Parse-SDK-iOS-OSX \
-  --module-version 1.15.0 \
+  --root-url http://parseplatform.org/Parse-SDK-iOS-OSX/api/ \
+  --module-version ${ver} \
   --framework-root . \
   --umbrella-header Parse/Parse.h \
-  --sdk iphonesimulator\
+  --sdk=iphonesimulator \
+  --exclude=./Bolts/* \
+  --theme fullwidth \
+  --skip-undocumented \
   --module Parse
+
+rm -rf ./Bolts # cleanup temporary bolts
