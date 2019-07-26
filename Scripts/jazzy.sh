4cc5af2875c9057178172ab49f8c6bb522da888f
Nathan Kellert <nathankellert@gmail.com>
2019-07-26 15:13:36 -0500

    Generate API for ParseUI

--- Scripts/jazzy.sh
+++ Scripts/jazzy.sh
@@ -18,4 +18,20 @@ jazzy \
   --framework-root Parse \
   --output docs/api
 
+jazzy \
+--objc \
+--clean \
+--author "Parse Community" \
+--author_url http://parseplatform.org \
+--github_url https://github.com/parse-community/Parse-SDK-iOS-OSX \
+--root-url http://parseplatform.org/Parse-SDK-iOS-OSX/ParseUI/api \
+--module-version ${ver} \
+--theme fullwidth \
+--skip-undocumented \
+--exclude=./Bolts/* \
+--module ParseUI \
+--umbrella-header ParseUI/Other/ParseUI.h \
+--framework-root ParseUI \
+--output docs/api
+
 rm -rf ./Parse/Bolts # cleanup temporary bolts
