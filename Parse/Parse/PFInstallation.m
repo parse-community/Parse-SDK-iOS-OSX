247379f8942278c5e977c72b3ff257c8d7c926a9
Thomas Kollbach <thomas@kollba.ch>
2019-11-12 13:30:45 +0100

    Remove bundle ID prefix for mac catalyst

--- Parse/Parse/PFInstallation.m
+++ Parse/Parse/PFInstallation.m
@@ -293,6 +293,17 @@ static NSSet *protectedKeys;
     NSString *appName = appInfo[(__bridge NSString *)kCFBundleNameKey];
     NSString *appVersion = appInfo[(__bridge NSString *)kCFBundleVersionKey];
     NSString *appIdentifier = appInfo[(__bridge NSString *)kCFBundleIdentifierKey];
+
+    // Mac Catalyst Apps use a prefix to the bundle ID. This should not be transmitted
+    // to the parse backend. Catalyst apps should look like iOS apps otherwise
+    // push and other services don't work properly.
+    if (@available(macCatalyst 13.0, *) && appIdentifier) {
+        NSRange macCatalystPrefix = [appIdentifier rangeOfString:@"maccatalyst."];
+        if (macCatalystPrefix.location == 0) {
+            appIdentifier = [appIdentifier stringByReplacingCharactersInRange:macCatalystPrefix
+                                                                   withString:@""];
+        }
+    }
     // It's possible that the app was created without an info.plist and we just
     // cannot get the data we need.
     // Note: it's important to make the possibly nil string the message receptor for
