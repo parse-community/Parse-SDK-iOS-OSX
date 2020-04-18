f9e67a948b5884e8af39c23bae1e57bdd4e592fb
Nathan Kellert <nathankellert@gmail.com>
2020-04-18 12:19:02 -0500

    adding bundle exec pod repo update

--- Scripts/publish.sh
+++ Scripts/publish.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -e
-pod repo update
+bundle exec pod repo update
 EXPANDED_CODE_SIGN_IDENTITY="-" EXPANDED_CODE_SIGN_IDENTITY_NAME="-" bundle exec pod trunk push Parse.podspec --allow-warnings
