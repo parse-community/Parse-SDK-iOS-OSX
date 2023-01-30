02f466acef11bcb8e62953c3983afd94b466938c
Manuel Trezza <5673677+mtrezza@users.noreply.github.com>
2023-01-30 03:12:14 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -17,9 +17,7 @@ let package = Package(
         .library(name: "ParseUI", targets: ["ParseUI"])
     ],
     dependencies: [
-        //.package(url: "https://github.com/parse-community/Bolts-ObjC.git", from: "1.10.0"),
-        // .package(url: "https://github.com/vazarkevych/Bolts-ObjC.git", branch: "spm"),
-        .package(url: "https://github.com/mtrezza/Bolts-ObjC.git", branch: "spm-rebase"),
+        .package(url: "https://github.com/parse-community/Bolts-ObjC.git", from: "1.10.0"),
         .package(url: "https://github.com/facebook/facebook-ios-sdk.git", from: "15.1.0")
     ],
     targets: [
