b03463be6770c81e7d28f8193c88bfea6dbc9464
Diamond Lewis <findlewis@gmail.com>
2023-10-05 13:40:07 -0500

    Revert "test tvos"
    This reverts commit 94735b9b565482bc13d752aeff7f2eeb4c8dc2e1.

--- ParseStarterProject/tvOS/ParseStarterProject-Swift/ParseStarter/AppDelegate.swift
+++ ParseStarterProject/tvOS/ParseStarterProject-Swift/ParseStarter/AppDelegate.swift
@@ -8,7 +8,8 @@
  */
 
 import UIKit
-import ParseCore
+
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
--- ParseStarterProject/tvOS/ParseStarterProject-Swift/ParseStarter/ViewController.swift
+++ ParseStarterProject/tvOS/ParseStarterProject-Swift/ParseStarter/ViewController.swift
@@ -8,6 +8,7 @@
  */
 
 import UIKit
+import Parse
 
 class ViewController: UIViewController {
 
