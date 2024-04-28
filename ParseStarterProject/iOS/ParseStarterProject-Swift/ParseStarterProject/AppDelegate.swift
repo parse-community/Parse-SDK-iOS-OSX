139eca7f2423bf92c5b6d821eaf4cda16816dd6f
Diamond Lewis <findlewis@gmail.com>
2024-04-28 09:39:16 -0500

    feat: Remove `ParseUI` (#1783)
    BREAKING CHANGE: This release removes `ParseUI`.

--- ParseStarterProject/iOS/ParseStarterProject-Swift/ParseStarterProject/AppDelegate.swift
+++ ParseStarterProject/iOS/ParseStarterProject-Swift/ParseStarterProject/AppDelegate.swift
@@ -12,9 +12,6 @@ import UserNotifications
 
 import ParseCore
 
-// If you want to use any of the UI components, uncomment this line
-// import ParseUI
-
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
