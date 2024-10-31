cd7e496f47f83e050630aa43ea84bdb48c9f9bea
Diamond Lewis <findlewis@gmail.com>
2024-10-31 14:41:26 -0500

    fix tests

--- ParseStarterProject/watchOS/ParseStarterProject-Swift/ParseStarterProject/AppDelegate.swift
+++ ParseStarterProject/watchOS/ParseStarterProject-Swift/ParseStarterProject/AppDelegate.swift
@@ -60,7 +60,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 noPushPayload = options[UIApplication.LaunchOptionsKey.remoteNotification.rawValue] != nil
             }
             if oldPushHandlerOnly || noPushPayload {
-                PFAnalytics.trackAppOpened(launchOptions: launchOptions)
+                PFAnalytics.trackAppOpenedWithLaunchOptions(inBackground: launchOptions)
             }
         }
 
@@ -106,7 +106,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didReceiveRemoteNotification userInfo: [NSObject : AnyObject]) {
         PFPush.handle(userInfo)
         if application.applicationState == UIApplication.State.inactive {
-            PFAnalytics.trackAppOpened(withRemoteNotificationPayload: userInfo)
+            PFAnalytics.trackAppOpenedWithRemoteNotificationPayload(inBackground: userInfo)
         }
     }
 
