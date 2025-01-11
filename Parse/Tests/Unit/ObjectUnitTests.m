bdb889dea37c9aabe722203b2e2fbd952c6b8165
Diamond Lewis <findlewis@gmail.com>
2025-01-10 18:40:02 -0600

    feat: Add support for `PFObject.objectFromDictionary`

--- Parse/Tests/Unit/ObjectUnitTests.m
+++ Parse/Tests/Unit/ObjectUnitTests.m
@@ -361,4 +361,13 @@
     XCTAssertNil(error);
 }
 
+- (void)testObjectFromDictionary {
+    NSDictionary *dict = @{ @"objectId": @"XYZ", @"score" : @1.0 };
+    PFObject *object = [PFObject objectFromDictionary:dict
+                                      defaultClassName:@"Test"];
+
+    XCTAssertEqualObjects(dict[@"objectId"], object.objectId);
+    XCTAssertEqualObjects(dict[@"score"], object[@"score"]);
+}
+
 @end
